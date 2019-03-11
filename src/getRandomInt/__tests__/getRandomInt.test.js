e3d28e0cdcc1f1891a8ad4b644a8418cfa664984
shaunsaker <shaun@aux.co.za>
2019-03-11 07:26:37 +0200

    Add getRandomIt test

---
+++ src/getRandomInt/__tests__/getRandomInt.test.js
@@ -0,0 +1,12 @@
+import getRandomInt from '..';
+
+describe('getRandomInt', () => {
+  it('should work', () => {
+    const min = 0;
+    const max = 10;
+    const randomInt = getRandomInt(min, max);
+
+    expect(randomInt).toBeGreaterThanOrEqual(min);
+    expect(randomInt).toBeLessThan(max);
+  });
+});
