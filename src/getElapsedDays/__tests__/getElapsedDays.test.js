201387f81f82dceb3927773edcb84863e8cbeeae
shaunsaker <shaun@aux.co.za>
2019-03-12 08:28:07 +0200

    Fix getElapsedDays test

--- src/getElapsedDays/__tests__/getElapsedDays.test.js
+++ src/getElapsedDays/__tests__/getElapsedDays.test.js
@@ -2,16 +2,10 @@ import getElapsedDays from '..';
 
 describe('getElapsedDays()', () => {
   it('should work', () => {
-    // Mock the date
-    const DATE_TO_USE = new Date(1552281656000);
-    const _Date = Date;
-    global.Date = jest.fn(() => DATE_TO_USE);
-    global.Date.UTC = _Date.UTC;
-    global.Date.parse = _Date.parse;
-    global.Date.now = _Date.now;
+    const time = Date.now();
+    const day = 1000 * 60 * 60 * 24; // ms * sec * min * hr
+    const twoDaysAgo = time - 2 * day;
 
-    const twoDaysAgo = 1552108856000;
-
-    expect(Math.round(getElapsedDays(twoDaysAgo))).toEqual(2);
+    expect(getElapsedDays(twoDaysAgo)).toEqual(2);
   });
 });
